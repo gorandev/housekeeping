12484c3434ae0b8d3b5b63be68b4c99e23ba6a55
Joaquin Perez <mondongo@gmail.com>
2018-03-27 14:22:58 -0300

    API worker: Added ensure block so that even if the job fails (maybe client takes too long to answer) the next job will be enqueued.

--- api/app/jobs/grab_tweets_job.rb
+++ api/app/jobs/grab_tweets_job.rb
@@ -11,24 +11,26 @@ class GrabTweetsJob < ApplicationJob
       config.access_token_secret = ENV.fetch('TWITTER_ACCESS_SECRET')
     end
 
-    Topic.all.each do |t|
-      client.search(t.querytext).take(10).collect do |tweet|
-        begin
-          Tweet.create!(
-            :topic => t,
-            :author => tweet.user.screen_name,
-            :body => tweet.full_text,
-            :retweets => tweet.retweet_count,
-            :favs => tweet.favorite_count,
-            :posted_on => tweet.created_at,
-            :twitter_id => tweet.id
-          )
-        rescue ActiveRecord::RecordNotUnique
-          # tweet already exists
+    begin
+      Topic.all.each do |t|
+        client.search(t.querytext).take(10).collect do |tweet|
+          begin
+            Tweet.create!(
+              :topic => t,
+              :author => tweet.user.screen_name,
+              :body => tweet.full_text,
+              :retweets => tweet.retweet_count,
+              :favs => tweet.favorite_count,
+              :posted_on => tweet.created_at,
+              :twitter_id => tweet.id
+            )
+          rescue ActiveRecord::RecordNotUnique
+            # tweet already exists
+          end
         end
       end
+    ensure
+      GrabTweetsJob.set(wait: 1.minute).perform_later
     end
-
-    GrabTweetsJob.set(wait: 1.minute).perform_later
   end
 end
