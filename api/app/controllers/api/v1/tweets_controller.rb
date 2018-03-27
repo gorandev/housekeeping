cf773ae073d1b823efc61b0b8be428df32c144a6
Joaquin Perez <mondongo@gmail.com>
2018-03-27 17:05:57 -0300

    API: Only serve the 10 latest tweets

--- api/app/controllers/api/v1/tweets_controller.rb
+++ api/app/controllers/api/v1/tweets_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::TweetsController < ApplicationController
       head :no_content and return
     end
 
-    render json: Tweet.eager_load(:topic).where(:topic => topic).order(posted_on: :desc).all
+    render json: Tweet.eager_load(:topic).where(:topic => topic).order(posted_on: :desc).limit(10)
   end
 end
